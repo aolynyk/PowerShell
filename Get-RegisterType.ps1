6fdf53ad5de642ab5952409e51eb2eadc86a6bb3
aolynyk <alex.olynyk@gmail.com>
2018-03-15 09:07:35 -0400

    Add files via upload

---
+++ Get-RegisterType.ps1
@@ -0,0 +1,27 @@
+﻿function Get-RegisterType {
+
+     [cmdletbinding()]
+
+     Param(
+
+     [Parameter(Mandatory=$True)]
+     [ValidateNotNullOrEmpty()]
+     [string]$computername=$env:COMPUTERNAME
+
+     )
+
+     if (Test-Connection -ComputerName $computername -Quiet -Count 1) {
+
+     Write-Host "$computername is online" -ForegroundColor Green -BackgroundColor Black
+          
+     $cs = Get-WmiObject -Class win32_ComputerSystem -ComputerName $computername 
+     $bios = Get-WmiObject -Class win32_Bios -ComputerName $computername 
+     $props = @{Computername = $computername
+                Model = $cs.model
+                Name = $cs.name
+                SerialNumber = $bios.serialnumber}
+     New-Object -Type PSObject -Prop $props
+     }
+     }
+
+; Get-RegisterType
---
+++ Repair-SophosUpdate.ps1
@@ -0,0 +1,25 @@
+﻿#RUN AS ADMIN
+function Repair-SophosUpdate {
+
+    [cmdletbinding()]
+
+    Param(
+
+         [Parameter(Mandatory=$True)]
+         [string]$computername
+    )
+    $Services = Get-Service -Name 'Sopho*'
+
+    ForEach ($Service in $Services) {
+
+        If (($Service.Name -like 'Sopho*') -and ($Service.Status -eq 'running')) {
+
+            Restart-Service -Name $Service.Name -PassThru -Verbose -Force
+         }
+        Else
+         {
+            Start-Service -Name $Service -PassThru -Verbose
+        }
+    }
+
+} ; Repair-SophosUpdate -Verbose
