7d143b07a4ac287b6fe1c951cbd36131ba63304c
aolynyk <alex.olynyk@gmail.com>
2018-03-16 16:51:52 -0400

    Add files via upload

---
+++ Install-PSCore.ps1
@@ -0,0 +1,22 @@
+﻿function Install-PSCore {
+
+[cmdletbinding()]
+
+param(
+
+[Parameter(Mandatory=$True)]
+[String]$ComputerName
+
+)
+
+$outfile = 'C:\users\aolynyk\Desktop\PowerShell-6.0.2-win-x64.msi'
+$filepath = 'C:\users\aolynyk\Desktop\PowerShell-6.0.2-win-x64.msi'
+$uri = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.2/PowerShell-6.0.2-win-x64.msi'
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Invoke-WebRequest -Uri $uri -UseBasicParsing -OutFile $outfile
+Start-Process -Wait -ArgumentList "/passive","/norestart", "/L*V c:\log\log.txt " -FilePath $filepath
+
+}
+; Install-PSCore
